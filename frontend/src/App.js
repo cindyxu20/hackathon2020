d4a91912fcbd4bd0999a9c8fa10abd379a22d0be
Ian Walston <ikwalston@comcast.net>
2020-12-05 14:26:16 -0500

    moved home screen components to HomeScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,10 @@
 import React from "react";
 
 // material-ui
-import { Box, Button, Typography } from "@material-ui/core"
 import { CssBaseline } from "@material-ui/core";
 import { createMuiTheme } from "@material-ui/core/styles";
 import { ThemeProvider } from "@material-ui/styles";
+import HomeScreen from "./components/HomeScreen"
 
 // duke remote experience
 import scenes from "../../public/data/scenes.json"
@@ -27,49 +27,10 @@ const dukeRemoteExperienceTheme = createMuiTheme({
 });
 function App() {
 
-    function handleClickStart() {
-        alert("lol")
-    }
-
     return (
         <ThemeProvider theme={dukeRemoteExperienceTheme}>
             <CssBaseline />
-
-            {/* title */}
-            <Box align="center" mt={10}>
-                <Typography>Remote Duke Experience</Typography>
-            </Box>
-
-            {/* button */}
-            <Box align="center" p={3}>
-                <Button
-                    onClick={handleClickStart}
-                    variant="contained"
-                    color="primary"
-                >
-                    start
-                </Button>
-            </Box>
-
-            <Box align="center" p={3}>
-                <Button
-                    variant="contained"
-                    color="secondary"
-                >
-                    credits
-                </Button>
-            </Box>
-
-            <Box align="center">
-
-                {JSON.stringify(scenes)}
-                {scenes.map(scene => {
-                    return <Box>
-                        <Typography variant="h3">{scene.name}</Typography>
-                    </Box>
-
-                })}
-            </Box>
+            <HomeScreen scenes={scenes} />
         </ThemeProvider>
     );
 }
--- frontend/src/components/HomeScreen.js
+++ frontend/src/components/HomeScreen.js
@@ -1,9 +1,42 @@
 import React from 'react'
+import { Box, Button, Typography } from "@material-ui/core"
 
-export default function HomeScreen() {
+export default function HomeScreen({scenes}) {
     return (
         <div>
-            
+            {/* title */}
+            <Box align="center" mt={10}>
+                <Typography>Remote Duke Experience</Typography>
+            </Box>
+
+            {/* button */}
+            <Box align="center" p={3}>
+                <Button
+                    variant="contained"
+                    color="primary"
+                >
+                    start
+                </Button>
+            </Box>
+
+            <Box align="center" p={3}>
+                <Button
+                    variant="contained"
+                    color="secondary"
+                >
+                    credits
+                </Button>
+            </Box>
+
+            <Box align="center">
+                <Typography>Scenes: </Typography>
+                {scenes.map(scene => {
+                    return <Box>
+                        <Typography variant="h3">{scene.name}</Typography>
+                    </Box>
+
+                })}
+            </Box>
         </div>
     )
 }
