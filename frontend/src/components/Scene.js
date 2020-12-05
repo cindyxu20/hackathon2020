faff801198234af690b98ec8558cb6cd14a38279
Joey Scarpa <joseph.scarpa@duke.edu>
2020-12-05 15:31:05 -0500

    delay time set to 200

--- frontend/src/components/Scene.js
+++ frontend/src/components/Scene.js
@@ -8,7 +8,7 @@ export default function Scene({ scene }) {
         <Box mb={5}>
             <Typography variant="h6">{scene.name}</Typography>
             <img src={scene.backgroundUrl} width="60px" />
-            <Carousel>
+            <Carousel timeout =  {200}>
                 {
                     scene.textContent.map(dialog => <Typography>{dialog.text}</Typography>)
                 }
