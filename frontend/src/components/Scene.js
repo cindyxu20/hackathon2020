a97c8131a2415e056cfa31dfb193d30b7ec530f9
Joey Scarpa <joseph.scarpa@duke.edu>
2020-12-05 15:44:55 -0500

    image size increased to 800px

--- frontend/src/components/Scene.js
+++ frontend/src/components/Scene.js
@@ -7,7 +7,7 @@ export default function Scene({ scene }) {
     return (
         <Box mb={5}>
             <Typography variant="h6">{scene.name}</Typography>
-            <img src={scene.backgroundUrl} width="60px" />
+            <img src={scene.backgroundUrl} width="800px" />
             <Carousel timeout =  {200}>
                 {
                     scene.textContent.map(dialog => <Typography>{dialog.text}</Typography>)
