c1f541e7b248a1d7e11bc9d46b93d199b2d7cb13
Ian Walston <ikwalston@comcast.net>
2020-12-05 17:47:40 -0500

    background image css props

--- frontend/src/components/HomeScreen.js
+++ frontend/src/components/HomeScreen.js
@@ -5,22 +5,23 @@ import { Link } from 'react-router-dom'
 // material-ui
 import { Box, Button, Typography } from "@material-ui/core"
 
-// duke remote experience
-import scenes from "../../../public/data/scenes.json"
-
-import Scene from "./Scene"
-
 export default function HomeScreen() {
 
     return (
-        <div style = {{height: "100vh", background:"url(./img/front-page-entrance.jpg)"}}>
+        <div style={{
+            height: "100vh",
+            background: "url(./img/front-page-entrance.jpg)",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+        }}>
             {/* title */}
-           
-            <Box align="center" mt={10}>
-                <Typography  color="whiteness" variant = "h2">Welcome to the Remote University Experience</Typography>
-                <Typography  color="whiteness" variant = "h6">Insights about studying in COVID times at Duke, as well as information applicable to students from other universities.</Typography>
+
+            <Box align="center">
+                <Typography color="whiteness" variant="h2">Welcome to the Remote University Experience</Typography>
+                <Typography color="whiteness" variant="h6">Insights about studying in COVID times at Duke, as well as information applicable to students from other universities.</Typography>
             </Box>
-            
+
             {/* button */}
             <Box align="center" p={3}>
                 <Button
@@ -41,7 +42,7 @@ export default function HomeScreen() {
                     credits
                 </Button>
             </Box>
-            
+
         </div>
     )
 }
