425ae410671118366a579ac305e062d5226f3a73
Ian Walston <ikwalston@comcast.net>
2020-12-06 01:08:11 -0500

    devil position state

--- frontend/src/components/BlueDevil.js
+++ frontend/src/components/BlueDevil.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Fade, Zoom } from "@material-ui/core"
 
 export default function BlueDevil() {
+
+    const [ top, setTop ] = useState(50)
+    const [ left, setLeft ] = useState(50)
+
+    const imgStyles = {
+        width: "300px",
+    }
+
+    const rootStyles = {
+        position: "fixed",
+        top: `${top}%`,
+        left: `${left}%`,
+    }
+
     return (
-        <img src="./img/mascot.gif"  width="240px"/>
+        <div style={rootStyles}>
+            <Fade in={true} timeout={3141}>
+                <img src="./img/mascot.gif" style={imgStyles} />
+            </Fade>
+        </div>
     )
 }
