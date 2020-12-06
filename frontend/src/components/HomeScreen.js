f343a37129cb94dd2d3f87b7aaabcef2f97ceb0a
Joey Scarpa <joseph.scarpa@duke.edu>
2020-12-05 23:09:47 -0500

    random

--- frontend/src/components/HomeScreen.js
+++ frontend/src/components/HomeScreen.js
@@ -1,48 +1,63 @@
 // react
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 // material-ui
-import { Box, Button, Typography } from "@material-ui/core"
+import { Box, Button, Typography } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 
 export default function HomeScreen() {
-
-    return (
-        <div class = "overlay" style={{height: "100vh", background: "url(./img/front-page-entrance.jpg)", backgroundPosition: "bottom", backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
-            {/* title */}
-            <Grid container style={{height: "100vh"}} direction="row" justify="center" alignItems="center">
-                <Grid item>
-                <Box   bgcolor="primary">
-                <Box align="center">
-                    <Typography color="whiteness" variant="h4">Welcome to the Remote University Experience</Typography>
-                    <Typography color="whiteness" variant="h6">Insights about studying in COVID times at Duke, as well as information applicable to students from other universities.</Typography>
-                </Box>
-
-                {/* button */}
-                <Box align="center" p={3}>
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        component={Link}
-                        to={"/scene"}
-                    >
-                        start  
-                </Button>
-                </Box>
-
-                <Box align="center" p={3}>
-                    <Button
-                        variant="contained"
-                        color="secondary"
-                    >
-                        credits
-                </Button>
-                </Box>
+  return (
+    <div
+      class="overlay"
+      style={{
+        height: "100vh",
+        background: "url(./img/front-page-entrance.jpg)",
+        backgroundPosition: "bottom",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      {/* title */}
+      <Grid
+        container
+        style={{ height: "100vh" }}
+        direction="row"
+        justify="center"
+        alignItems="center"
+      >
+        <Grid item>
+          <Box bgcolor="primary">
+            <Box align="center">
+              <Typography color="whiteness" variant="h4">
+                Welcome to the Remote University Experience
+              </Typography>
+              <Typography color="whiteness" variant="h6">
+                Insights about studying in COVID times at Duke, as well as
+                information applicable to students from other universities.
+              </Typography>
             </Box>
-                </Grid>
-            
-            </Grid>
-        </div>
-    )
+
+            {/* button */}
+            <Box align="center" p={3}>
+              <Button
+                variant="contained"
+                color="primary"
+                component={Link}
+                to={"/scene"}
+              >
+                start
+              </Button>
+            </Box>
+
+            <Box align="center" p={3}>
+              <Button variant="contained" color="secondary">
+                credits
+              </Button>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
