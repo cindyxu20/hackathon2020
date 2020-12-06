17f6403b6ab6c635ba4e442ff920c6344db23beb
Joey Scarpa <joseph.scarpa@duke.edu>
2020-12-05 23:30:09 -0500

    credits implemented

--- frontend/src/components/HomeScreen.js
+++ frontend/src/components/HomeScreen.js
@@ -38,7 +38,6 @@ export default function HomeScreen() {
               </Typography>
             </Box>
 
-<<<<<<< HEAD
             {/* button */}
             <Box align="center" p={3}>
               <Button
@@ -50,34 +49,10 @@ export default function HomeScreen() {
                 start
               </Button>
             </Box>
-=======
-    return (
-        <div class = "overlay" style={{height: "100vh", background: "url(./img/front-page-entrance.jpg)", backgroundPosition: "bottom", backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
-            {/* title */}
-            <Grid container style={{height: "100vh"}} direction="row" justify="center" alignItems="center">
-                <Grid item>
-                <Box   bgcolor="primary">
-                <Box align="center">
-                    <Typography color="whiteness" variant="h4">Welcome to the Remote University Experience</Typography>
-                    <Typography color="whiteness" variant="h6">Explore Duke while gaining insights, advice, and resources to help you succeed in an online learning environment!</Typography>
-                </Box>
-
-                {/* button */}
-                <Box align="center" p={3}>
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        component={Link}
-                        to={"/scene"}
-                    >
-                        start  
-                </Button>
-                </Box>
->>>>>>> 9eac9f2f6be41db3bf5d4fcb42b91b6f7997ecde
 
             <Box align="center" p={3}>
-              <Button variant="contained" color="secondary">
-                credits
+              <Button href = "credit.html" variant="contained" color="secondary">
+                  Credits
               </Button>
             </Box>
           </Box>
---
+++ public/credits.js
@@ -0,0 +1,51 @@
+let yPos;
+let athena;
+let cindy;
+let eric;
+let ian;
+let joey;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  yPos = windowHeight;
+  athena = new Credit("Athena Yao", "character designer and script writer", 200);
+  cindy = new Credit("Cindy Xu", "not-so-sketchy artist and script writer", 400);
+  eric = new Credit("Eric Quan", "sound splicer and Pokemon correspondent", 600);
+  ian = new Credit("Ian Walston", "programmer and person of knowedge", 800);
+    joey = new Credit("Joseph Scarpa", "team dunce and somehow last alphabetically", 1000);
+   
+}
+
+
+function draw() {
+  background(1, 33, 105);
+  textAlign(CENTER);
+  textFont('EB Garamond');
+  textSize(100);
+  text("Credits" , windowWidth/2, yPos);
+  athena.display();
+  cindy.display();
+    eric.display();
+      ian.display();
+        joey.display();
+yPos--;
+
+}
+
+
+class Credit {
+  constructor(c, description, diff) {
+    this.c = c;
+    this.description = description;
+    this.diff = diff;
+  }
+  display() {
+    fill(255, 255, 255);
+  textSize(75)
+  text(this.c, width/2, yPos + this.diff);
+  textSize(50)
+  text(this.description, width/2, yPos + this.diff + 50);
+
+  }
+
+}
